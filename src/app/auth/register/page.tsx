163487fd8ea6db55576f7bbfcd3d27b644e8e96e
tasneemmansy <tasnimelagamy2023@gmail.com>
2026-06-30 20:06:20 +0300

    register form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,4 +1,5 @@
 import AuthNav from "../../Shared/components/AuthNav/AuthNav";
+import RegisterForm from "./RegisterForm";
 
 export default function Register() {
   return (
@@ -6,6 +7,7 @@ export default function Register() {
       <div className="w-full">
         <h1 className="text-[#C5D86D] text-[25px]">Create your account and start using QuizWiz!</h1>
         <AuthNav/>
+        <RegisterForm/>
       </div>
     </>
   )
