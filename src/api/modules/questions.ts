5740390680d090b5d5c90c72180be14b0af7168b
Mohamed Elhussainey <mw61887@gmail.com>
2026-07-08 04:24:46 +0300

    adding Question Form (Add , Edit , View)

--- src/api/modules/questions.ts
+++ src/api/modules/questions.ts
@@ -1,7 +1,20 @@
+import { QuestionFormValues } from "@/src/types/questions";
 import axiosClient from "../axiosClient";
 
 export const GetAllQuestions = ()=>{
     return axiosClient.get("/question");
+}
+
+export const GetQuestionById = (id: string)=>{
+    return axiosClient.get(`/question/${id}`);
+}
+
+export const CreateQuestion = (data: QuestionFormValues)=>{
+    return axiosClient.post("/question", data);
+}
+
+export const UpdateQuestion = (id: string, data: QuestionFormValues)=>{
+    return axiosClient.put(`/question/${id}`, data);
 }
 
 export const DeleteQuestion = (id: string)=>{
